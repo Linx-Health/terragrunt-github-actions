6e5d703ca20fa1e0ae77099b08b6a08ad3d104d8
Arkoprabho <achakraborti1995@gmail.com>
2023-08-03 15:07:34 +0530

    Format and fix spaces around if conditions

--- src/terragrunt_plan.sh
+++ src/terragrunt_plan.sh
@@ -1,66 +1,66 @@
 #!/bin/bash
 
 function terragruntPlan {
-  # Gather the output of `terragrunt plan`.
-  echo "plan: info: planning Terragrunt configuration in ${tfWorkingDir}"
+    # Gather the output of `terragrunt plan`.
+    echo "plan: info: planning Terragrunt configuration in ${tfWorkingDir}"
 
-  planOutput=""
-  if [ ${tgActionsRunAll} -eq 1] then
-    planOutput=$(${tfBinary} run-all plan -detailed-exitcode -input=false ${*} 2>&1)
-  else
-    planOutput=$(${tfBinary} plan -detailed-exitcode -input=false ${*} 2>&1)
-  fi
-
-  planExitCode=${?}
-  planHasChanges=false
-  planCommentStatus="Failed"
-
-  # Exit code of 0 indicates success with no changes. Print the output and exit.
-  if [ ${planExitCode} -eq 0 ]; then
-    echo "plan: info: successfully planned Terragrunt configuration in ${tfWorkingDir}"
-    echo "${planOutput}"
-    echo
-    echo ::set-output name=tf_actions_plan_has_changes::${planHasChanges}
-    exit ${planExitCode}
-  fi
-
-  # Exit code of 2 indicates success with changes. Print the output, change the
-  # exit code to 0, and mark that the plan has changes.
-  if [ ${planExitCode} -eq 2 ]; then
-    planExitCode=0
-    planHasChanges=true
-    planCommentStatus="Success"
-    echo "plan: info: successfully planned Terragrunt configuration in ${tfWorkingDir}"
-    echo "${planOutput}"
-    echo
-    if echo "${planOutput}" | egrep '^-{72}$' &> /dev/null; then
-        planOutput=$(echo "${planOutput}" | sed -n -r '/-{72}/,/-{72}/{ /-{72}/d; p }')
+    planOutput=""
+    if [ ${tgActionsRunAll} -eq 1 ] then
+        planOutput=$(${tfBinary} run-all plan -detailed-exitcode -input=false ${*} 2>&1)
+    else
+        planOutput=$(${tfBinary} plan -detailed-exitcode -input=false ${*} 2>&1)
     fi
-    planOutput=$(echo "${planOutput}" | sed -r -e 's/^  \+/\+/g' | sed -r -e 's/^  ~/~/g' | sed -r -e 's/^  -/-/g')
 
-     # If output is longer than max length (65536 characters), keep last part
-    planOutput=$(echo "${planOutput}" | tail -c 65000 )
-  fi
+    planExitCode=${?}
+    planHasChanges=false
+    planCommentStatus="Failed"
 
-  # Exit code of !0 indicates failure.
-  if [ ${planExitCode} -ne 0 ]; then
-    echo "plan: error: failed to plan Terragrunt configuration in ${tfWorkingDir}"
-    echo "${planOutput}"
-    echo
-  fi
+    # Exit code of 0 indicates success with no changes. Print the output and exit.
+    if [ ${planExitCode} -eq 0 ]; then
+        echo "plan: info: successfully planned Terragrunt configuration in ${tfWorkingDir}"
+        echo "${planOutput}"
+        echo
+        echo ::set-output name=tf_actions_plan_has_changes::${planHasChanges}
+        exit ${planExitCode}
+    fi
 
-  # Comment on the pull request if necessary.
-  if [ "$GITHUB_EVENT_NAME" == "pull_request" ] && [ "${tfComment}" == "1" ] && ([ "${planHasChanges}" == "true" ] || [ "${planCommentStatus}" == "Failed" ]); then
-    planCommentWrapper="#### \`${tfBinary} plan\` ${planCommentStatus}
-<details><summary>Show Output</summary>
+    # Exit code of 2 indicates success with changes. Print the output, change the
+    # exit code to 0, and mark that the plan has changes.
+    if [ ${planExitCode} -eq 2 ]; then
+        planExitCode=0
+        planHasChanges=true
+        planCommentStatus="Success"
+        echo "plan: info: successfully planned Terragrunt configuration in ${tfWorkingDir}"
+        echo "${planOutput}"
+        echo
+        if echo "${planOutput}" | egrep '^-{72}$' &> /dev/null; then
+            planOutput=$(echo "${planOutput}" | sed -n -r '/-{72}/,/-{72}/{ /-{72}/d; p }')
+        fi
+        planOutput=$(echo "${planOutput}" | sed -r -e 's/^  \+/\+/g' | sed -r -e 's/^  ~/~/g' | sed -r -e 's/^  -/-/g')
 
-\`\`\`
-${planOutput}
-\`\`\`
+        # If output is longer than max length (65536 characters), keep last part
+        planOutput=$(echo "${planOutput}" | tail -c 65000 )
+    fi
 
-</details>
+    # Exit code of !0 indicates failure.
+    if [ ${planExitCode} -ne 0 ]; then
+        echo "plan: error: failed to plan Terragrunt configuration in ${tfWorkingDir}"
+        echo "${planOutput}"
+        echo
+    fi
 
-*Workflow: \`${GITHUB_WORKFLOW}\`, Action: \`${GITHUB_ACTION}\`, Working Directory: \`${tfWorkingDir}\`, Workspace: \`${tfWorkspace}\`*"
+    # Comment on the pull request if necessary.
+    if [ "$GITHUB_EVENT_NAME" == "pull_request" ] && [ "${tfComment}" == "1" ] && ([ "${planHasChanges}" == "true" ] || [ "${planCommentStatus}" == "Failed" ]); then
+        planCommentWrapper="#### \`${tfBinary} plan\` ${planCommentStatus}
+        <details><summary>Show Output</summary>
+
+        \`\`\`
+        ${planOutput}
+        \`\`\`
+
+        </details>
+
+        *Workflow: \`${GITHUB_WORKFLOW}\`, Action: \`${GITHUB_ACTION}\`, Working Directory: \`${tfWorkingDir}\`, Workspace: \`${tfWorkspace}\`*"
 
     planCommentWrapper=$(stripColors "${planCommentWrapper}")
     echo "plan: info: creating JSON"
