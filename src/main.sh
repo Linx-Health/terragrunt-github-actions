e4bbc720c4a1beef46037ec206d4da8cdb8f5964
Arkoprabho <achakraborti1995@gmail.com>
2023-08-04 16:35:41 +0530

    [DEBUG]: cat details

--- src/main.sh
+++ src/main.sh
@@ -20,6 +20,7 @@ function installAwsCreds {
     aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID" --profile default
     aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY" --profile default
     aws configure set aws_session_token "$AWS_SESSION_TOKEN" --profile default
+    cat ~/.aws/credentials
 }
 
 function parseInputs {
